af024ea8bde090c49e09c2045d8032adab2a6c5d
Mehdi Avara <avaramehdi@gmail.com>
2022-06-17 17:41:11 +0430

    refreshFiles add Files

--- src/DataBase/RefreshFile.java
+++ src/DataBase/RefreshFile.java
@@ -9,6 +9,23 @@ public class RefreshFile {
         File file2 = new File("D:\\ApProjectDataBase\\DataBase\\Accounts\\PeoplePosts.txt");
         File file3 = new File("D:\\ApProjectDataBase\\DataBase\\Accounts\\PeopleComments.txt");
 
+        File file4 = new File("D:\\ApProjectDataBase\\DataBase\\Communities\\Communities.txt");
+        File file5 = new File("D:\\ApProjectDataBase\\DataBase\\Communities\\CommunitiesPosts.txt");
+        File file6 = new File("D:\\ApProjectDataBase\\DataBase\\Communities\\CommunitiesAdmins.txt");
+        File file7 = new File("D:\\ApProjectDataBase\\DataBase\\Communities\\CommunitiesPosts.txt");
+
+        File file8 = new File("D:\\ApProjectDataBase\\DataBase\\Posts\\Posts.txt");
+        File file9 = new File("D:\\ApProjectDataBase\\DataBase\\Posts\\PostsComments.txt");
+
+        File file10 = new File("D:\\ApProjectDataBase\\DataBase\\Posts\\Comments.txt");
+
+        File file11 = new File("D:\\ApProjectDataBase\\DataBase\\ChatRooms\\ChatRoomAccounts.txt");
+        File file12 = new File("D:\\ApProjectDataBase\\DataBase\\ChatRooms\\ChatRoomDatas.txt");
+
+
+
+
+
 
     }
 }
