b886355b3538e66bbf316cff2c17c6ac5beca738
Mehdi Avara <avaramehdi@gmail.com>
2022-06-17 17:20:17 +0430

    dataBase starts

--- src/DataBase/DataBase.java
+++ src/DataBase/DataBase.java
@@ -1,4 +1,20 @@
 package DataBase;
 
+import java.util.HashMap;
+
 public class DataBase {
+    static private final HashMap<String,Controller> dataBase =new HashMap<String, Controller>();
+    static private DataBase singleTone;
+    static public DataBase getDataBase(){
+        if (singleTone == null) {
+            singleTone = new DataBase();
+        }
+        return singleTone;
+    }
+    void addDataBase(String str,Controller controller){
+        dataBase.put(str,controller);
+    }
+    Controller getControllert(String str){
+        return dataBase.get(str);
+    }
 }
