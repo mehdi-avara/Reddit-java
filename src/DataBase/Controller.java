06b613dbe75336dc41534ac7c7a9e220107fdef2
Mehdi Avara <avaramehdi@gmail.com>
2022-06-17 18:53:16 +0430

    Controller constructor

--- src/DataBase/Controller.java
+++ src/DataBase/Controller.java
@@ -1,4 +1,15 @@
 package DataBase;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Controller {
+    private FileWriter fileWriter;
+    Controller(String fileName){
+        try {
+            fileWriter = new FileWriter(fileName,true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
