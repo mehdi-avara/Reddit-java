8c5251702ade2b0e431828d2916c198c222f3a0c
Mehdi Avara <avaramehdi@gmail.com>
2022-06-17 18:44:13 +0430

    RefreshFile now works correctly

--- src/DataBase/RefreshFile.java
+++ src/DataBase/RefreshFile.java
@@ -1,45 +1,66 @@
 package DataBase;
 
 import java.io.File;
+import java.io.FileWriter;
 
 public class RefreshFile {
     public static void main(String[] args) {
-        File file = new File("D:\\ApProjectDataBase\\DataBase\\Accounts\\PeopleAccounts.txt");
-        File file1 = new File("D:\\ApProjectDataBase\\DataBase\\Accounts\\PeopleCommunities.txt");
-        File file2 = new File("D:\\ApProjectDataBase\\DataBase\\Accounts\\PeoplePosts.txt");
-        File file3 = new File("D:\\ApProjectDataBase\\DataBase\\Accounts\\PeopleComments.txt");
+        File[] file = new File[13];
+        String[] texts = new String[13];
+        FileWriter[] fileWriters = new FileWriter[13];
+        file[0] = new File("D:\\ApProjectDataBase\\DataBase\\Accounts");
+        file[1] = new File("D:\\ApProjectDataBase\\DataBase\\Accounts");
+        file[2] = new File("D:\\ApProjectDataBase\\DataBase\\Accounts");
+        file[3] = new File("D:\\ApProjectDataBase\\DataBase\\Accounts");
 
-        File file4 = new File("D:\\ApProjectDataBase\\DataBase\\Communities\\Communities.txt");
-        File file5 = new File("D:\\ApProjectDataBase\\DataBase\\Communities\\CommunitiesPosts.txt");
-        File file6 = new File("D:\\ApProjectDataBase\\DataBase\\Communities\\CommunitiesAdmins.txt");
-        File file7 = new File("D:\\ApProjectDataBase\\DataBase\\Communities\\CommunitiesPosts.txt");
+        file[4] = new File("D:\\ApProjectDataBase\\DataBase\\Communities");
+        file[5] = new File("D:\\ApProjectDataBase\\DataBase\\Communities");
+        file[6] = new File("D:\\ApProjectDataBase\\DataBase\\Communities");
+        file[7] = new File("D:\\ApProjectDataBase\\DataBase\\Communities");
 
-        File file8 = new File("D:\\ApProjectDataBase\\DataBase\\Posts\\Posts.txt");
-        File file9 = new File("D:\\ApProjectDataBase\\DataBase\\Posts\\PostsComments.txt");
+        file[8] = new File("D:\\ApProjectDataBase\\DataBase\\Posts");
+        file[9] = new File("D:\\ApProjectDataBase\\DataBase\\Posts");
 
-        File file10 = new File("D:\\ApProjectDataBase\\DataBase\\Posts\\Comments.txt");
+        file[10] = new File("D:\\ApProjectDataBase\\DataBase\\Posts");
 
-        File file11 = new File("D:\\ApProjectDataBase\\DataBase\\ChatRooms\\ChatRoomAccounts.txt");
-        File file12 = new File("D:\\ApProjectDataBase\\DataBase\\ChatRooms\\ChatRoomDatas.txt");
-
-
-
-
-
-        File file4 = new File("D:\\ApProjectDataBase\\DataBase\\Communities\\Communities.txt");
-        File file5 = new File("D:\\ApProjectDataBase\\DataBase\\Communities\\CommunitiesPosts.txt");
-        File file6 = new File("D:\\ApProjectDataBase\\DataBase\\Communities\\CommunitiesAdmins.txt");
-        File file7 = new File("D:\\ApProjectDataBase\\DataBase\\Communities\\CommunitiesPosts.txt");
-
-        File file8 = new File("D:\\ApProjectDataBase\\DataBase\\Posts\\Posts.txt");
-        File file9 = new File("D:\\ApProjectDataBase\\DataBase\\Posts\\PostsComments.txt");
-
-        File file10 = new File("D:\\ApProjectDataBase\\DataBase\\Posts\\Comments.txt");
-
-        File file11 = new File("D:\\ApProjectDataBase\\DataBase\\ChatRooms\\ChatRoomAccounts.txt");
-        File file12 = new File("D:\\ApProjectDataBase\\DataBase\\ChatRooms\\ChatRoomData.txt");
+        file[11] = new File("D:\\ApProjectDataBase\\DataBase\\ChatRooms");
+        file[12] = new File("D:\\ApProjectDataBase\\DataBase\\ChatRooms");
 
 
+        texts[0] = "PeopleAccounts";
+        texts[1] = "PeopleCommunities";
+        texts[2] = "PeoplePosts";
+        texts[3] = "PeopleComments";
+
+        texts[4] = "Communities";
+        texts[5] = "CommunitiesPosts";
+        texts[6] = "CommunitiesAdmins";
+        texts[7] = "CommunitiesPosts";
+
+        texts[8] = "Posts";
+        texts[9] = "PostsComments";
+
+        texts[10] = "Comments";
+
+        texts[11] = "ChatRoomAccounts";
+        texts[12] = "ChatRoomData";
+        try{
+            for (File item :
+                    file) {
+             if (!item.exists()){
+                 item.mkdirs();
+             }
+            }
+        }catch (Exception ignored){}
+        try{
+            for (int i = 0; i < file.length; i++) {
+                File f = new File(file[i].getAbsoluteFile()+"\\"+texts[i]+".txt");
+                f.createNewFile();
+                fileWriters[i] = new FileWriter(f);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
 
 
