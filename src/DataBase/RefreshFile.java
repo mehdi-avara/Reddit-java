8fcdd7d7627054bb9bd3f4e59d3ffba768eeab9b
Mehdi Avara <avaramehdi@gmail.com>
2022-06-17 17:29:06 +0430

    RefreshFile starts

---
+++ src/DataBase/RefreshFile.java
@@ -0,0 +1,14 @@
+package DataBase;
+
+import java.io.File;
+
+public class RefreshFile {
+    public static void main(String[] args) {
+        File file = new File("D:\\ApProjectDataBase\\DataBase\\Accounts\\PeopleAccounts.txt");
+        File file1 = new File("D:\\ApProjectDataBase\\DataBase\\Accounts\\PeopleCommunities.txt");
+        File file2 = new File("D:\\ApProjectDataBase\\DataBase\\Accounts\\PeoplePosts.txt");
+        File file3 = new File("D:\\ApProjectDataBase\\DataBase\\Accounts\\PeopleComments.txt");
+
+
+    }
+}
