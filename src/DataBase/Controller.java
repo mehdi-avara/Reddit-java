a163809f3e1445d6f1e3523b30e4e98641befd1b
Mehdi Avara <avaramehdi@gmail.com>
2022-06-26 21:39:41 +0430

    add controller to for files

--- src/DataBase/Controller.java
+++ src/DataBase/Controller.java
@@ -1,9 +1,11 @@
 package DataBase;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.sql.Ref;
 
 public class Controller {
     private final File file;
@@ -70,5 +72,51 @@ public class Controller {
         }
         this.writeFile(sb.toString());
     }
+}
+class datasController{
+    private final String file;
+    private RandomAccessFile raf;
+    private FileWriter fw;
 
+    public datasController(String path) {
+        file = path;
+    }
+    public String readFile(String id){
+        try {
+            raf = new RandomAccessFile(new File(file+"\\"+id), "rw");
+        } catch (FileNotFoundException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        StringBuilder recovery = new StringBuilder();
+        String i;
+        try {
+            while ((i = raf.readLine()) != null) {
+                recovery.append(i + "\n");
+            }
+            raf.seek(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return recovery.toString();
+    }
+
+    public void write(String id,String text,boolean... reset){
+        try {
+            raf = new RandomAccessFile(new File(file+"\\"+id), "rw");
+        } catch (FileNotFoundException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        try {
+            if (reset.length != 0) {
+                fw = new FileWriter(file+"\\"+id);
+            }
+            fw.write(text);
+            fw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return;
+    }
 }
