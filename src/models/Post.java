9e6e326612160bac1bb675c0ac046ba3b1b340d8
Mehdi Avara <avaramehdi@gmail.com>
2022-06-27 20:04:29 +0430

    edit post

--- src/models/Post.java
+++ src/models/Post.java
@@ -1,7 +1,12 @@
 package models;
 
+import DataBase.DataBase;
+import DataBase.datasController;
+
 public class Post {
     private static int id = 1;
+    private int personId;
+    private int comId;
     private String title;
     private String description;
 
